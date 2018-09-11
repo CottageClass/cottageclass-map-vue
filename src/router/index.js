f2780cfe56ef3670696d22f52b01c0a531eb80b1
holmesworcester <hwilson@gmail.com>
2018-09-11 14:11:52 -0400

    adding splashes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import MainView from '@/components/MainView.vue'
 import Bookings from '@/components/Bookings'
 import EmergencyInfo from '@/components/EmergencyInfo'
+import Splash from '@/components/Splash.vue'
+import EnterCode from '@/components/EnterCode.vue'
 
 Vue.use(Router)
 
@@ -22,6 +24,16 @@ export default new Router({
     	path: 'emergency-info/:id',
     	name: 'emergencyInfo',
     	component: EmergencyInfo
-    }
+    },
+    {
+    	path: '/splash',
+    	name: 'Splash',
+    	component: Splash 
+    },
+    {
+    	path: '/enter-code',
+    	name: 'EnterCode',
+    	component: EnterCode
+    },    
   ]
 })
