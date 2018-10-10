8a18c9cbd5fb2b9794360dbdb196066a0893c1df
holmesworcester <hwilson@gmail.com>
2018-10-09 22:25:01 -0400

    Made whole button clickable

--- src/components/onboarding/Terms.vue
+++ src/components/onboarding/Terms.vue
@@ -5,7 +5,7 @@
       <p class="onb-paragraph-subheading-2">Please check the box at the bottom to agree to our terms before continuing</p>
     </div>
     <div class="onb-paragraph-align-left">Terms of service<br>Last updated: June 6, 2018<br>If you reside in the United States, please note that section 13 of these terms of service contains an arbitration clause and class action waiver. It affects how disputes with CottageClass are resolved. By accepting these terms of service, you agree to be bound by this arbitration provision. Please read it carefully.Please read these terms of service carefully as they contain important information regarding your legal rights, remedies and obligations. These include various limitations and exclusions, a clause that governs the jurisdiction and venue of disputes, and obligations to comply with applicable laws and regulations.In particular... (ADD LATEST TERMS OF SERVICE HERE)</div>
-    <div class="onb-fixed-bottom-actions-checkbox-container">
+    <div @click="toggle" class="onb-fixed-bottom-actions-checkbox-container">
       <div class="form-block-2 w-form">
         <form id="email-form" name="email-form" data-name="Email Form">
           <div class="checkbox-field w-checkbox"><input type="checkbox" id="checkbox" name="checkbox" data-name="Checkbox" class="w-checkbox-input" v-model="checked">
@@ -26,6 +26,11 @@ export default {
     return {
       checked: this.value.agreed,
       errorMsg: 'To continue, check the box below to agree to our terms of service.'
+    }
+  },
+  methods: {
+    toggle: function () {
+      this.checked = !this.checked
     }
   },
   mounted: function () {
