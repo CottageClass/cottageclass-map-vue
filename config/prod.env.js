e0834671c1961cbb576ca3ecb19fc421369bcf93
Frank Kotsianas <frankkotsianas@gmail.com>
2018-10-12 17:05:53 -0400

    Add prod server command, setting NODE_ENV on dev server
    - this should perhaps be set in the deploy command, unsure
    @holmesworcester

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL_API: 'https://cottageclass-app-api.herokuapp.com',
+  BASE_URL_API: '"https://cottageclass-app-api.herokuapp.com"',
 }
