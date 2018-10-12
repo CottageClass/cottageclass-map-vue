09c76a0146ae4a3cd315953bd58ef29db0e51edf
Frank Kotsianas <frankkotsianas@gmail.com>
2018-10-12 19:53:38 -0400

    Ship dev-server with Heroku API URL and just change locally for dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL_API: '"https://localhost:3000"',
+  //BASE_URL_API: '"https://localhost:3000"',
+  // - ship with Heroku URL, and change up locally for simplicity's sake
+  BASE_URL_API: '"https://cottageclass-app-api.herokuapp.com"',
 })
