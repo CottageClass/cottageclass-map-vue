d9c218152248b0f5cf64478c7cd3f2b2f9055797
holmesworcester <hwilson@gmail.com>
2018-10-15 11:19:11 -0400

    Made entire button clickable using just CSS
    (...fixing earlier race condition that prevented checking the box.)

--- src/components/onboarding/Terms.vue
+++ src/components/onboarding/Terms.vue
@@ -5,15 +5,16 @@
       <p class="onb-paragraph-subheading-2">Please check the box at the bottom to agree to our terms before continuing</p>
     </div>
     <div class="onb-paragraph-align-left">Terms of service<br>Last updated: June 6, 2018<br>If you reside in the United States, please note that section 13 of these terms of service contains an arbitration clause and class action waiver. It affects how disputes with CottageClass are resolved. By accepting these terms of service, you agree to be bound by this arbitration provision. Please read it carefully.Please read these terms of service carefully as they contain important information regarding your legal rights, remedies and obligations. These include various limitations and exclusions, a clause that governs the jurisdiction and venue of disputes, and obligations to comply with applicable laws and regulations.In particular... (ADD LATEST TERMS OF SERVICE HERE)</div>
-    <div class="onb-fixed-bottom-actions-checkbox-container">
+    <label><div class="onb-fixed-bottom-actions-checkbox-container">
       <div class="form-block-2 w-form">
         <form id="email-form" name="email-form" data-name="Email Form">
           <div class="checkbox-field w-checkbox"><input type="checkbox" id="checkbox" name="checkbox" data-name="Checkbox" class="w-checkbox-input" v-model="checked">
-            <label for="checkbox" class="checkbox-label w-form-label">I agree to the terms</label>
+            <div class="checkbox-label w-form-label">I agree to the terms</div>
           </div>
         </form>
       </div>
     </div>
+  </label>
   </div>
 </template>
 
