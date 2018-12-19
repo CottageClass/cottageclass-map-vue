648fa2c6b84122d36e78d4988779917cfa0e3db2
holmesworcester <hwilson@gmail.com>
2018-12-19 16:06:13 -0500

    Updates location text to make it more welcoming

--- src/components/onboarding/Location.vue
+++ src/components/onboarding/Location.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="onb-content-container _100vh">
     <div class="onb-top-content-container">
-      <h1 class="onb-heading-large">Where do you live?</h1>
-      <p class="onb-paragraph-subheading-2">Please enter your address so we can list your event &amp; find events near you.</p>
+      <h1 class="onb-heading-large">What's your home address?</h1>
+      <p class="onb-paragraph-subheading-2">Please enter your address so we can list your activity &amp; find activities near you.</p>
     </div>
     <div class="onb-location-search-container">
       <div class="w-form">
@@ -11,7 +11,7 @@
             ref="address"
             id="map"
             classname="email-form-2 w-form location-text-field w-input"
-            placeholder="e.g. 10 Main St."
+            placeholder="e.g. 10 Main St. Anytown, NY"
             v-on:placechanged="getAddressData"
             country="us"
         >
