31e437715fab0c73e2c386d3b059b2892a3bc7c2
holmesworcester <hwilson@gmail.com>
2019-01-25 08:44:54 -0500

    Adds wrapper component for onboarding / editing questions

---
+++ src/components/onboarding/Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content-container">
+    <div class="title-container">
+      <h1 class="title">{{ title }}</h1>
+    </div>
+    <div class="location-search-container">
+    	<slot></slot>
+    </div>
+    <p class="paragraph-small-50">{{ explanation }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Question',
+	props: ['title', 'explanation']
+}
+</script>
