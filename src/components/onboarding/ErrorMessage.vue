5bb48e75e3bf18efd38f9c07e3f5b5f60564e09e
holmesworcester <hwilson@gmail.com>
2019-01-25 08:46:05 -0500

    Adds component for error messages

---
+++ src/components/onboarding/ErrorMessage.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="onb-error-container">
+	<div class="onb-error-text">{{ text }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  name: 'ErrorMessage'
+}
+</script>
