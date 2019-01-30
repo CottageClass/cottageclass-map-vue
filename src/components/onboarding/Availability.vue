92d07f2fb8ad4d1b60dad1d413334933c1f37488
Ari Kardasis <ari.kardasis@gmail.com>
2019-01-30 10:52:26 -0500

    moved availability into computed so it refreshes

--- src/components/onboarding/Availability.vue
+++ src/components/onboarding/Availability.vue
@@ -3,17 +3,16 @@
  title="When are you available for childcare in a pinch?"
  subtitle="Other parents may text you to request care during the times you choose. There's never any obligation to respond, so just pick times when you're usually available to provide care."
  explanation="You'll be able to accept or decline any requests. Most families list days and times that they are generally at home with their own children or doing a fun or educational activity which they'd like to include other children in.">
-   <MultipleChoice 
-   type="checkbox" 
-   v-model="availability" 
-   :labelsAndOrder="labelsAndOrder"/> 
+   <MultipleChoice
+   type="checkbox"
+   v-model="availability"
+   :labelsAndOrder="labelsAndOrder"/>
   </Question>
 </template>
 
 <script>
 import Question from '@/components/onboarding/Question.vue'
 import MultipleChoice from '@/components/onboarding/MultipleChoice.vue'
-
 
 export default {
 
@@ -22,12 +21,11 @@ export default {
   components: { Question, MultipleChoice },
   data () {
     return {
-      availability: this.value,
-      labelsAndOrder: [ 
-        [ 'availableMornings', '9am - 3pm'],
-        [ 'availableAfternoons', '3pm - 7pm'],
-        [ 'availableEvenings', 'After 7pm'],
-        [ 'availableWeekends', 'Weekends']
+      labelsAndOrder: [
+        [ 'availableMornings', '9am - 3pm' ],
+        [ 'availableAfternoons', '3pm - 7pm' ],
+        [ 'availableEvenings', 'After 7pm' ],
+        [ 'availableWeekends', 'Weekends' ]
       ]
     }
   },
@@ -39,6 +37,9 @@ export default {
       },
       deep: true
     }
+  },
+  computed: {
+    availability: function () { return this.value }
   }
 }
 </script>
