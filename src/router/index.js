82eda786a11e5e4c2d3d3a43e82dcabddc3c0963
holmesworcester <hwilson@gmail.com>
2019-02-12 15:24:12 -0500

    Better url for user profiles

--- src/router/index.js
+++ src/router/index.js
@@ -65,7 +65,7 @@ export default new Router({
       component: ProfileEdit
     },
     {
-      path: '/profile/:id',
+      path: '/users/:id',
       name: 'ProviderProfile',
       component: ProviderProfile,
       props: true
