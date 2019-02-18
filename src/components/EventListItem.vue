52d5bad04a5a3c98f84abadf4637d0c0f8dc4b72
Ari Kardasis <ari.kardasis@gmail.com>
2019-02-18 16:05:37 -0500

    change facebook id to uid everywhere

--- src/components/EventListItem.vue
+++ src/components/EventListItem.vue
@@ -19,7 +19,7 @@
         </div>
     <div class="action-bar">
           <div class="host-info">
-            <router-link :to="{ name: 'ProviderProfile', params: { id: event.hostId }}"><AvatarImage class="avatar-small" :person="{facebookId: event.hostFacebookUid, avatar: event.hostAvatar}" /></router-link>
+            <router-link :to="{ name: 'ProviderProfile', params: { id: event.hostId }}"><AvatarImage class="avatar-small" :person="{facebookUid: event.hostFacebookUid, avatar: event.hostAvatar}" /></router-link>
             <div class="text-block">
               Hosted by
               <router-link :to="{ name: 'ProviderProfile', params: { id: event.hostId }}" class="host-name link">{{ event.hostFirstName }}</router-link> &amp;
