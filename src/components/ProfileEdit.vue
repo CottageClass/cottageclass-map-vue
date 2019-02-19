0433c92f295df195e1a0282d45d7e37432d5ee0e
holmesworcester <hwilson@gmail.com>
2019-02-19 16:19:21 -0500

    Updates local state from backend, after saving

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -119,6 +119,7 @@ export default {
         this.saveButtonText = 'Saving...'
         api.submitUserInfo(this.currentUser.id, this.phone, this.location, this.availability, this.children, this.currentUser).then(res => {
           this.saveButtonText = ' \u2714 Saved'
+          this.$store.dispatch('establishCurrentUserAsync', Token.currentUserId(this.$auth))
           console.log('user update SUCCESS')
           console.log(res)
           return res
