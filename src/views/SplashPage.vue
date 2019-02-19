2961af09553793edc0042807d0e7eb45fd662cc2
Ari Kardasis <ari.kardasis@gmail.com>
2019-02-19 09:46:30 -0500

    move facebook authentication into mixin for reuse

--- src/views/SplashPage.vue
+++ src/views/SplashPage.vue
@@ -346,11 +346,12 @@ import RsvpButton from '@/components/RsvpButton.vue'
 import MainNav from '@/components/MainNav.vue'
 import Footer from '@/components/Footer.vue'
 import { mapGetters } from 'vuex'
-import * as Token from '@/utils/tokens'
+import providerAuth from '@/mixins/ProviderAuthentication'
 
 export default {
   name: 'SplashPage',
   components: { RsvpButton, MainNav, Footer },
+  mixins: [providerAuth],
   data () {
     return {
       networks: networks,
@@ -370,42 +371,8 @@ export default {
       return ['(iPhone|iPod|iPad)(?!.*Safari)'].every(expression => {
         return !!navigator.userAgent.match(new RegExp(`(${expression})`, 'ig'))
       })
-    },
-    authenticate: function (provider) {
-      /*
-   *  Logs in the user (Facebook)
-   * - follows OAuth flow using VueAuth to get OAuth code
-   * - sends code to backend to exchange for access_token
-   * - backend fetches access_token, stores it in DB, and sends back JWT for user
-   * - VueAuthenticate stores JWT for future API access authorization
-   */
-      console.log('authing')
-      // store value of this to access this.$emit during callback
-      let component = this
-      this.$auth.authenticate(provider)
-        .then(res => {
-          console.log('auth SUCCESS')
-          return this.$store.dispatch('establishCurrentUserAsync', Token.currentUserId(component.$auth))
-        }).then(() => {
-          if (this.currentUser.hasAllRequiredFields && !this.rsvpAttemptedId) {
-            // redirect to home screen if they haven't attempted an RSVP
-            this.$router.push({ name: 'Home' })
-          } else if (this.currentUser.hasAllRequiredFields && !!this.rsvpAttemptedId) {
-            // confirm that they want to RSVP if they have attempted an RSVP
-            this.$router.push({ name: 'RsvpConfirmation', params: { eventId: this.rsvpAttemptedId } })
-          } else if (this.currentUser.id) {
-            // begin onboarding
-            this.$router.push({ name: 'OnboardNewUser' })
-          } else {
-            return false
-          }
-        }).catch(function (err) {
-          console.log('auth FAILURE or user not onboarded yet')
-          console.log(err)
-        })
-    },
-  },
-  computed: mapGetters(['currentUser'])
+    }
+  }
 }
 
 </script>
